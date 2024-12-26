c031deabc36d720f0fe10b1d25e7ccc76bb8efc5
atna <atay.narynov@gmail.com>
2024-12-26 15:05:44 +0600

    Remove unused code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,7 @@
-import React from 'react';
-// import { ReactComponent as GithubIcon } from '../../assets/github.svg';
-import { GithubLink, HeaderContainer, Title, HeaderIconsContainer } from './styed';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppStore } from '../../store/store';
-import { toggleDarkMode } from '../../store/reducers/appReducer';
+import React from "react";
 
 const Header: React.FC = () => {
-  const dispatch = useDispatch();
-  const isDarkMode = useSelector((state: AppStore) => state.app.darkMode);
-
-  return (
-    <div style={{height: 48}}></div>
-    // <HeaderContainer>
-    //   <Title>React Weather</Title>
-    //   <HeaderIconsContainer>
-    //     <DarkModeToggle checked={isDarkMode} onChange={() => dispatch(toggleDarkMode())} size={60} />
-    //     <GithubLink href="http://www.github.com/esnz/reactweather">
-    //       <GithubIcon />
-    //     </GithubLink>
-    //   </HeaderIconsContainer>
-    // </HeaderContainer>
-  );
+  return <div style={{ height: 48 }}></div>;
 };
 
 export default Header;
