f3f70c2b1cc407c363fe3e5a5f8bd42a4305fad7
atna <atay.narynov@gmail.com>
2025-02-26 10:39:43 +0600

    refactor: Change link's style

--- src/components/PopularCities/PopularCities.tsx
+++ src/components/PopularCities/PopularCities.tsx
@@ -6,8 +6,51 @@ import { useParams } from "react-router-dom";
 const CardContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  gap: 15px 20px;
+  gap: 15px 15px;
   margin: 0 0 20px 0;
+`;
+
+const CardButton = styled.a`
+  align-items: center;
+  appearance: none;
+  background-color: #3eb2fd;
+  background-image: linear-gradient(1deg, #4f58fd, #149bf3 99%);
+  background-size: calc(100% + 20px) calc(100% + 20px);
+  border-radius: 100px;
+  border-width: 0;
+  box-shadow: none;
+  box-sizing: border-box;
+  color: #ffffff;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: CircularStd, sans-serif;
+  font-size: 1rem;
+  height: auto;
+  justify-content: center;
+  line-height: 1.5;
+  padding: 6px 20px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transition: background-color 0.2s, background-position 0.2s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: top;
+  white-space: nowrap;
+
+  &:active,
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    background-position: -20px -20px;
+  }
+
+  &:focus:not(:active) {
+    box-shadow: rgba(40, 170, 255, 0.25) 0 0 0 0.125em;
+  }
 `;
 
 const PopularCities = () => {
@@ -16,9 +59,9 @@ const PopularCities = () => {
   return (
     <CardContainer>
       {Object.values(popularCities).map((city: CityData) => (
-        <a key={city.name} href={`/${locale}/city/${city.url}`}>
+        <CardButton key={city.name} href={`/${locale}/city/${city.url}`}>
           {city.name}
-        </a>
+        </CardButton>
       ))}
     </CardContainer>
   );
